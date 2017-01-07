3986fe934f5ee5543622a72b71e12b37b593379f
DenisDubinin <dadubinin@gmail.com>
2017-01-07 20:09:41 +0100

    add triangle

--- OpenGL_House/main.cpp
+++ OpenGL_House/main.cpp
@@ -1,15 +1,226 @@
-//
-//  main.cpp
-//  OpenGL_House
-//
-//  Created by apple on 2017-01-07.
-//  Copyright © 2017 apple. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include <cassert>
+#include <fstream>
 
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+// MACROS
+#define NUM_ARRAY_ELEMENTS(a) sizeof(a) / sizeof(*a)
+
+using namespace std;
+using namespace glm;
+
+// variables declaration
+GLFWwindow* window;
+GLuint WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+int viewportWidth, viewportHeight;
+const char* title = "Dream house";
+
+typedef struct {
+    vec3 vertices;
+    vec3 color;
+} GeometryData;
+
+// function declaration
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+GLuint LoadShaders(const char* vertexFilePath, const char* fragmentFilePath);
+
+// geometry defenitional
+GeometryData geometryData[] = {
+    vec3(-0.5f, -0.5f, 0.0f),
+    vec3(+1.0f,  0.0f, 0.0f),
+    
+    vec3( 0.5f, -0.5f, 0.0f),
+    vec3(+1.0f,  0.0f, 0.0f),
+    
+    vec3( 0.0f,  0.5f, 0.0f),
+    vec3( 1.0f,  0.0f, 0.0f),
+};
+
+GLushort indices[] = { 0, 1, 2 };
+
+GLuint numVertices = NUM_ARRAY_ELEMENTS(geometryData);
+GLuint numIndices = NUM_ARRAY_ELEMENTS(indices);
+
+// Buffers declaration
+GLuint vertexBufferObjectID, vertexArrayObjectID, elementBufferObjectID;
+GLuint programID;
+
+
+
+// MAIN
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    assert(glfwInit() && "glfw init error");
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, title, NULL, NULL);
+    assert(window && "Failed to create window");
+    glfwMakeContextCurrent(window);
+    
+    /*--------glew options-----------*/
+    glewExperimental = true;
+    assert(glewInit() == GLEW_OK && "Failad init GLEW");
+    
+    glfwGetFramebufferSize(window, &viewportWidth, &viewportHeight);
+    glViewport(0, 0, viewportWidth, viewportHeight);
+    
+    /*--------glfw options-----------*/
+    glfwSetKeyCallback(window, key_callback);
+    
+    
+    /* ------ GL buffers-------------*/
+    glGenVertexArrays(1, &vertexArrayObjectID);
+    glGenBuffers(1, &vertexBufferObjectID);
+
+    glBindVertexArray(vertexArrayObjectID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjectID);
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(geometryData), geometryData, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(vec3), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(vec3), (GLvoid*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    // unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+    /* ----- load programID ---------*/
+    programID = LoadShaders("shaders/house.vs.glsl", "shaders/house.frag.glsl");
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Draw geometry
+        glUseProgram(programID);
+        glBindVertexArray(vertexArrayObjectID);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if(key == GLFW_KEY_ESCAPE && GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+}
+
+GLuint LoadShaders(const char* vertexFilePath, const char* fragmentFilePath) {
+
+    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint ProgramID = glCreateProgram();
+    
+    GLint Result = GL_FALSE;
+    int InfoLogLength;
+    string line = "";
+    
+    // read vertexCode
+    string vertexFileCode;
+    ifstream vertexFileStream;
+    vertexFileStream.open(vertexFilePath);
+    assert(!vertexFileStream.fail() && "Can't open vertex file");
+    while (getline(vertexFileStream, line)) {
+        if(vertexFileStream.fail()) {
+            assert("Fail to read line");
+            vertexFileStream.close();
+        } else {
+            vertexFileCode += "\n" + line;
+        }
+    }
+    vertexFileStream.close();
+    line = "";
+    
+    // read fragmentCode
+    string fragmentFileCode;
+    ifstream fragmentFileStream;
+    fragmentFileStream.open(fragmentFilePath);
+    assert(!fragmentFileStream.fail() && "Can't open fragment file");
+    while (getline(fragmentFileStream, line)) {
+        if(fragmentFileStream.fail()) {
+            assert("Fail to read line");
+            fragmentFileStream.close();
+        } else {
+            fragmentFileCode += "\n" + line;
+        }
+    }
+    fragmentFileStream.close();
+    
+    // Compile Vertex Shader
+    cout << "Compiling shader: " << vertexFilePath << endl;
+    char const * vertexSourcePointer = vertexFileCode.c_str();
+    glShaderSource(VertexShaderID, 1, &vertexSourcePointer , NULL);
+    glCompileShader(VertexShaderID);
+    
+    // Check Vertex Shader
+    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if (InfoLogLength > 0) {
+        vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+        glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+        cout  << &VertexShaderErrorMessage[0] << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    // Compile Fragment Shader
+    cout << "Compiling shader: " << fragmentFilePath << endl;
+    char const * FragmentSourcePointer = fragmentFileCode.c_str();
+    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
+    glCompileShader(FragmentShaderID);
+    
+    // Check Fragment Shader
+    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if (InfoLogLength > 0) {
+        vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+        glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+        cout  << &FragmentShaderErrorMessage[0] << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    // Link the program
+    cout <<  "Linking program" << endl;
+    glAttachShader(ProgramID, VertexShaderID);
+    glAttachShader(ProgramID, FragmentShaderID);
+    glLinkProgram(ProgramID);
+    
+    // Check the program
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+    if (InfoLogLength > 0) {
+        vector<char> ProgramErrorMessage(InfoLogLength + 1);
+        glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        cout  << &ProgramErrorMessage[0] << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    // Free memory
+    glDetachShader(ProgramID, VertexShaderID);
+    glDetachShader(ProgramID, FragmentShaderID);
+    
+    glDeleteShader(VertexShaderID);
+    glDeleteShader(FragmentShaderID);
+    
+    return ProgramID;
+}
+
